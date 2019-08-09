bc23560e45fdddaff1bd566d17c53b2c9b94be40
Vlaja97 <v.vlajic.vlada@gmail.com>
2019-08-09 14:14:28 -0700

    Creating title and description fields

--- includes/classes/VideoDetailsFormProvider.php
+++ includes/classes/VideoDetailsFormProvider.php
@@ -2,8 +2,12 @@
 class VideoDetailsFormProvider {
     public function createUploadform() {
         $fileInput = $this->createFileInput();
-        return "<form action='processing.php' method='POST'
+        $titleInput = $this->createTitleInput();
+        $descriptionInput = $this->createDescriptionInput();
+        return "<form action='processing.php' method='POST'>
                     $fileInput
+                    $titleInput
+                    $descriptionInput
                 </form>";
     }
 
@@ -11,9 +15,22 @@ class VideoDetailsFormProvider {
      
         return "<div class='form-group'>
             <label for='exampleFormControlFile1'>Your File</label>
-            <input type='file' class='form-control-file' id='exampleFormControlFile1' required>
+            <input type='file' class='form-control-file' id='exampleFormControlFile1' name='fileInput' required>
             </div>";
-            }
+    }
+
+    private function createTitleInput() {
+        return "<div class='form-group'> 
+                    <input class='form-control' type='text' placeholder='Title' name='titleInput'>
+                </div>";  
+    }
+
+    private function createDescriptionInput() {
+        return "<div class='form-group'> 
+                    <textarea class='form-control' placeholder='Description' name='descriptionInput' rows='3'></textarea>
+                </div>";  
+    }
+     
 }
 
 ?>
