9fe48449febbe5131c6c1a810cbf2d717fea956c
cvetkoS <cvetko.stevic97@gmail.com>
2019-08-09 18:01:09 +0200

    Added empty index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>VideoTube</title>
+</head>
+<body>
+    <div id="pageContainer">
+
+        <div id="mastHeadContainer">
+            
+        </div>
+
+        <div id="sideNavContainer">
+
+        </div>
+
+        <div id="mainSectionContainer">
+            
+        </div>
+    </div>
+</body>
+</html>
