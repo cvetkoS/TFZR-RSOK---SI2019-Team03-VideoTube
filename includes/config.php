d36e1c8706c43d7f4b811d16283cb05e91521f0b
Vlaja97 <v.vlajic.vlada@gmail.com>
2019-08-09 15:09:12 -0700

    Creating Database Creating config.php file which is included in header file

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+ob_start(); //Turns on output buffering
+
+date_default_timezone_set("Europe/Belgrade");
+
+try {
+    $con = new PDO("mysql:dbname=VideoTube;host=localhost", "root", ""); //Connection to the Database
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); //Error handling and error output
+}
+catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
