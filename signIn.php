ed1a787d25ab1008656ae73840b4fbb1c17cc96d
Vlaja97 <v.vlajic.vlada@gmail.com>
2019-08-10 14:07:58 -0700

    Created signIn.php file

---
+++ signIn.php
@@ -0,0 +1,37 @@
+<?php require_once("includes/config.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>VideoTube</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <div class="signInContainer">
+        <div class="column">
+            <div class="header">
+                 <img src="assets/images/icons/VideoTubeLogo.png" title="Site Logo" alt="Site Logo">
+                 <h3>Sign Up</h3>
+                 <span>to continiue to VideoTube</span>
+            </div>
+            <div class="loginForm">
+                    
+            </div>
+
+            <a class="signInMessage" href="signIn.php">Already have an account? Sign in here!</a>
+        </div>
+    </div>
+</body>
+</html>
