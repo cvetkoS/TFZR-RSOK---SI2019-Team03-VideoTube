8312adf8ce62f710fb7d961241fc3c3ece5344c5
cvetkoS <cvetko.stevic97@gmail.com>
2019-08-10 16:30:59 +0200

    Added processing.php page

---
+++ processing.php
@@ -0,0 +1,35 @@
+<?php 
+require_once("includes/header.php");
+require_once("includes/classes/VideoUploadData.php");
+require_once("includes/classes/VideoProcessor.php");
+
+
+//uploadButton matches the name in the createUploadButton() function in VideoDetailsFormProvider.php
+//if someone went manually to this page, it will show this error.
+//User should be accessing this page only when submitting video upload form
+if (!isset($_POST["uploadButton"])) {
+    echo "No file sent";
+    exit();
+}
+
+// 1. ------------------Create file upload data
+//parameters are name labels from the VideoDetailsFormProvider form
+$videoUploadData = new VideoUploadData($_FILES["fileInput"],
+                                       $_POST["titleInput"],
+                                       $_POST["descriptionInput"],
+                                       $_POST["privacyInput"],
+                                       $_POST["categoryInput"],
+                                       "REPLACE-THIS");
+
+// 2. ------------------Process video data(upload)
+$videoProcessor = new VideoProcessor($con);
+$wasSuccessful = $videoProcessor->upload($videoUploadData);
+
+// 3. ------------------Check if upload was successful
+
+?>
+
+
+
+<?php require_once("includes/footer.php"); ?>
+
