5b5bc81dd8ace4f433d0ea2b9a80ff5236cf19b6
Ana <tasicana996@gmail.com>
2019-08-17 16:02:51 +0200

    Styling the search page

--- includes/classes/VideoGrid.php
+++ includes/classes/VideoGrid.php
@@ -74,7 +74,7 @@ class VideoGrid
 
             $newUrl = basename($_SERVER["PHP_SELF"]) . "?" . $newQuery;
 
-            $filter = "<div class='videoGridHeader'>
+            $filter = "<div class='right'>
                             <span>Order by:</span>
                             <a href='$newUrl&orderBy=uploadDate'>Upload date</a>
                             <a href='$newUrl&orderBy=views'>Most viewed</a>
