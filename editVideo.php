d5921bdd019c97a900c4bc346705c8a7aaff70e8
DraganaGajin <gajin.dragana@gmail.com>
2019-08-19 16:56:11 +0200

    Edit video page

---
+++ editVideo.php
@@ -0,0 +1,25 @@
+<?php
+require_once("includes/header.php");
+require_once("includes/classes/VideoPlayer.php");
+require_once("includes/classes/VideoDetailsFormProvider.php");
+require_once("includes/classes/VideoUploadData.php");
+
+if(!User::isLoggedIn()){
+    header("Location: signIn.php");
+}
+
+/*prvobitno je pisalo videoId ali se ne poklapa sa klasom Video
+ pa je moralo da se menja u samom URL-u u videoId, da ne bi stranica davala gresku
+ da je nedefinisano*/
+
+if(!isset($_GET["videoid"])) {
+    echo "No video selected";
+    exit();
+}
+
+$video = new Video($con, $_GET["videoid"], $userLoggedInObj);
+if($video->getUploadedBy() !=$userLoggedInObj->getUsername()) {
+    echo "Not your video";
+    exit();
+}
+?>
